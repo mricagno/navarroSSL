214943ba918f794306d879c3a7f83631d7acb035
Leandro Navarro <lelenavarro@hotmail.com>
2016-04-05 13:42:49 -0300

    Ejercicio 1.6+1.7
    Ejercicio 1.6+1.7 valor de EOF

---
+++ Ej1.6.c
@@ -0,0 +1,16 @@
+/* 
+Navarro Leandro Adrian
+05/04/2016
+1.6 + 1.7
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	
+    int var;
+	
+	var = (getchar()!= EOF);
+
+    printf("Valor de EOF = %d \n", var);
+}

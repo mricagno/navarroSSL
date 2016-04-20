f3bd21ae06dc8bae90cd06b13e01c8f80d95919b
Leandro Navarro <lelenavarro@hotmail.com>
2016-04-20 17:03:52 -0300

    Ejercicio 2.5 + correcion fecha 2.4
    Ejercicio 2.5 + correcion fecha 2.4

--- EjLibro/Ej2.5.c
+++ EjLibro/Ej2.5.c
@@ -1,7 +1,8 @@
 /* 
 Navarro Leandro Adrian
 20/04/2016
-Ej2.4 Escriba una versión alterna de squeeze(sl,s2) que borre cada carácter de si que coincida con cualquier carácter de la cadena s2
+Ej2.5 Escriba la función any(sl,s2), que regresa la primera posición de la cadena si en donde se encuentre cualquier carácter de la cadena s2, 
+o -1 si no contiene caracteres de s2.
 */
 
 #include <stdio.h>
@@ -9,7 +10,7 @@ Ej2.4 Escriba una versión alterna de squeeze(sl,s2) que borre cada carácter de
 #define MAXLINE 1000
 
 int get_line(char line[], int maxline);
-void squeeze(char s1[], char s2[]);
+int any(char s1[], char s2[]);
 
 int main(void)
 {
@@ -17,14 +18,13 @@ int main(void)
     char s1[MAXLINE];
     char s2[MAXLINE];
 
-    printf("Frase 1:\n");
+    printf("Cadena:\n");
     while ((len = get_line(s1, MAXLINE)) == 0);
 
-    printf("Frase 2:\n");
+    printf("Caracter a buscar:\n");
     while ((len = get_line(s2, MAXLINE)) == 0);
 
-    squeeze(s1, s2);
-    printf("Frase filtrada:\n", s1);
+    printf("El primer match es: %d\n", any(s1, s2));
 
     return 0;
 }
@@ -41,20 +41,20 @@ int get_line(char s[], int lim)
     return l;
 }
 
-void squeeze(char s1[], char s2[])
+int any(char s1[], char s2[])
 {
-    int i, j, k;
+    int i, j;
 
     i = 0;
     while (s2[i] != '\0') {
         j = 0;
         while (s1[j] != '\0') {
-            if (s1[j] == s2[i]) {
-                k = j;
-                while ((s1[k] = s1[++k]) != '\0');
-            } else
-                ++j;
+            if (s1[j] == s2[i])
+                return j;
+            ++j;
         }
         ++i;
     }
+
+    return -1;
 }
